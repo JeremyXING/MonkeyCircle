50000af4f9700f5799570c47ae63b41f49894b52
gyf94 <gina_doris@sina.com>
2016-06-10 11:09:36 +0800

    增加挑战和问题的MODEL

---
+++ MonkeyCircle/DBUtility/SqlHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections;
+
+namespace DBUtility
+{
+    public abstract class SqlHelper
+    {
+        private static Hashtable parmCache = Hashtable.Synchronized(new Hashtable());
+    }
+}
---
+++ MonkeyCircle/Model/Challenge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Challenge
+    {
+        private int chanID;//挑战序号
+        private int companyID;
+        private int chanType;
+        private int salary;
+        private bool open;
+        private DateTime publishTime;
+        private int questionID;
+        private int acceptNum;
+        private int finishNum;
+        private int lastAcceptNum;
+        private int lastFinishNum;
+        private bool status;
+        private float passPer;
+    }
+}
